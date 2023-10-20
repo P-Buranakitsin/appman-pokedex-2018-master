92457e40a7391595773f79959478c6b0edadc906
Pavaruth Buranakitsin <pavaruth.buranakitsin@gmail.com>
2023-10-20 22:54:49 +0700

    Empty Pokedex done!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
 const COLORS = {
   Psychic: "#f8a5c2",
@@ -12,16 +12,53 @@ const COLORS = {
   Lightning: "#f9ca24",
   Darkness: "#574b90",
   Colorless: "#FFF",
-  Fire: "#eb4d4b"
-}
+  Fire: "#eb4d4b",
+  bottomBarBackground: "#ec5656",
+  colorAddButton: "#dc7777",
+  bottomBarTextColor: "#ffffff",
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1 style={{height: "10%"}}>My Pokedex</h1>
+        <div className="Content" style={{height: "80%"}}>
+
+        </div>
+        <div
+          style={{
+            backgroundColor: COLORS.bottomBarBackground,
+            width: "100%",
+            height: "10%",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: COLORS.colorAddButton,
+              borderRadius: "50%",
+              padding: 40,
+              position: "absolute",
+              bottom: 0,
+              left: "50%",
+              cursor: "pointer",
+              transform: "translate(-50%)",
+              width: 50,
+              height: 50,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              color: COLORS.bottomBarTextColor,
+              fontSize: 90
+            }}
+          >
+            +
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
